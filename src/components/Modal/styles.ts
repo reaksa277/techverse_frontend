b288e5b572b345515811290bb77132834a09378e
reaksa <chhimchanreaksa@gmail.com>
2025-12-01 00:16:05 +0700

    feat(login): add modal login and style

---
+++ src/components/Modal/styles.ts
@@ -0,0 +1,11 @@
+export const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 380,
+  bgcolor: "background.paper",
+  borderRadius: 3,
+  boxShadow: 24,
+  p: 3,
+};
